b3bed01cd6fc4159f1400d34a5f952c361d8c50b
AbdelrahmanGamal <abdelrhman.gamal53@gmail.com>
2025-07-10 05:45:25 +0300

    remove unnecessary logging

--- src/utils/clearCookieValue.ts
+++ src/utils/clearCookieValue.ts
@@ -1,7 +1,6 @@
 import { Response } from 'express';
 
 const clearCookieValue = (res: Response, cookieName: string) => {
-  console.log('visited');
   res.clearCookie(cookieName, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
